7fe16f1a157bb90adb113341bf14678a7e8017f4
pumpuiy <narachai.p@gmail.com>
2019-03-22 12:12:37 +0700

    first commit

---
+++ src/main/java/com/websystique/spring/AppMain.java
@@ -0,0 +1,12 @@
+package com.websystique.spring;
+
+import org.springframework.context.support.AbstractApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class AppMain {
+
+	public static void main(String args[]){
+		AbstractApplicationContext context = new ClassPathXmlApplicationContext("app-config.xml");
+	}
+	
+}
---
+++ src/main/java/com/websystique/spring/scheduling/MyBean.java
@@ -0,0 +1,11 @@
+package com.websystique.spring.scheduling;
+
+import org.springframework.stereotype.Component;
+
+@Component("myBean")
+public class MyBean {
+
+	public void printMessage() {
+		System.out.println("I am called by Spring scheduler");
+	}
+}
